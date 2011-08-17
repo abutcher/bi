e1111010c6eb3161547e99721e1176625b58b886
Andrew Butcher <abutcher@afrolegs.com>
2011-08-16 21:14:41 -0400

    setup.py for idea pkg

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = "idea",
+    version = "0.01",
+    description = "Iterative Dichotomization on Every Attribute, Data Mining Toolkit",
+    package_dir = {'' : 'lib'},
+    packages = ['idea'],
+    )
